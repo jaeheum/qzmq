3ea7c8dd334ddc2c5b4717d1419126646a975747
Jay Han (한재흠) <jaeheum@me.com>
2012-08-20 00:41:24 -0700

    Adding perl version, mreq.pl and correcting example data in mreq.py.

--- demos/mreq.py
+++ demos/mreq.py
@@ -9,6 +9,6 @@ req = ctx.socket(zmq.REQ)
 req.connect("tcp://127.0.0.1:5559")
 while 1:
     time.sleep(1.0)
-    data = ''.join(random.sample(string.letters, 10))
+    data = ''.join(random.sample('qwertyuiop', 10))
     req.send_string(data)
     print "python (pid=%s): received the oginal data: %s" % (os.getpid(), req.recv_string())
